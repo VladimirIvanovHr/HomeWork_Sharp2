d4ccb2a42743a563c8b61cc6459a1b72a5f0d968
Vladimir Ivanov <ivanofvv9@gmail.com>
2022-10-10 01:20:20 +0300

    Задача на определение выходного дня

---
+++ HW_2_3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int dayweek = Convert.ToInt32(Console.ReadLine());
+if (dayweek<1 || dayweek> 7)
+    {
+        Console.WriteLine("неверное число");
+    }
+else 
+    {
+        if (dayweek>0 && dayweek<6)
+        {
+           Console.WriteLine("нет, это будний день");
+        }
+        else
+        {
+            Console.WriteLine("да, это выходной день");
+        }
+    }
