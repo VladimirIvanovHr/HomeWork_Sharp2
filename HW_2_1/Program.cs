094ab7c1f3305219f02a97f5dddbb6ca9a3c2656
Vladimir Ivanov <ivanofvv9@gmail.com>
2022-10-10 00:44:19 +0300

    Задача на определение 2-го числа в трехначном

---
+++ HW_2_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+if (threeDigitNumber < 100)
+    {
+        Console.WriteLine("неверное число");
+    }
+else 
+    {if (threeDigitNumber > 999) 
+        {
+            Console.WriteLine("неверное число");
+        }
+    else
+            {
+                string stringNumber = Convert.ToString(threeDigitNumber);
+                Console.WriteLine("вторая цифра этого числа -> " + stringNumber[1]); 
+            }
+    }
