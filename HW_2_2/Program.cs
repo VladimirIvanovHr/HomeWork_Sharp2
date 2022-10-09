93672f0ad162dc28bd04df5b206b8afe034bea31
Vladimir Ivanov <ivanofvv9@gmail.com>
2022-10-10 00:52:43 +0300

    Задача на вывод третьей цифры

---
+++ HW_2_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string result = Convert.ToString(number);
+if (result.Length > 2)
+    {
+        Console.WriteLine("третья цифра -> " + result[2]);
+    }
+else 
+    {
+        Console.WriteLine("-> третьей цифры нет");
+    }
